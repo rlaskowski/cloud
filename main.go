d400335ac9b778b9fc46d09bb228666016845bc4
r.laskowski <rafal.laskowski@currenda.pl>
2018-10-19 11:07:11 +0200

    Poprawa pliku wdrożenia

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+type User struct {
+	Name string
+}
 
 func main() {
 
-	fmt.Println("Test cloud")
+	http.HandleFunc("/", test)
+	appengine.Main()
+
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+
+	var user = User{
+		Name: "Test",
+	}
+
+	jsonFile, _ := json.Marshal(user)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonFile)
 
 }
